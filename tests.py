20ae00d63d38797bd6fdea824183ecec7e323fc0
Chase Sterling <chase.sterling@gmail.com>
2013-04-11 16:57:46 -0400

    Start refactoring LazyProxy tests

--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+import operator
 import json
 import sys
 import unittest
@@ -11,8 +12,11 @@ PY3 = sys.version_info[0] >= 3
 
 if PY3:
     long = int
+    div = operator.truediv
     def cmp(a, b):
         return (a > b) - (a < b)
+else:
+    div = operator.div
 
 
 class TestRefLoading(unittest.TestCase):
@@ -112,77 +116,65 @@ class TestDereferencer(unittest.TestCase):
 
 class ProxyTestMixin:
 
-    def checkInteger(self, v):
-        p = self.proxied(v)
-        self.assertEqual(p|0b010101, v|0b010101)
-        self.assertEqual(p&0b010101, v&0b010101)
-        self.assertEqual(p^0b010101, v^0b010101)
-        self.assertEqual(~p, ~v)
-        self.assertEqual(p<<3, v<<3)
-        self.assertEqual(p>>2, v>>2)
+    def check_func(self, func, value, arg=None, reversable=False):
+        """
+        Checks func works the same with `value` as when `value` is proxied.
 
-        self.assertEqual(0b010101|p, 0b010101|v)
-        self.assertEqual(0b010101&p, 0b010101&v)
-        self.assertEqual(0b010101^p, 0b010101^v)
-        self.assertEqual(3<<p, 3<<v)
-        self.assertEqual(2>>p, 2>>v)
-        for f in hex, oct:
-            self.assertEqual(f(p), f(v))
+        """
 
-        self.checkNumeric(v)
+        p = self.proxied(value)
+        if arg is not None:
+            self.assertEqual(func(p, arg), func(value, arg))
+            if reversable:
+                self.assertEqual(func(arg, p), func(arg, value))
+        else:
+            self.assertEqual(func(p), func(value))
 
-    def checkNumeric(self, v):
-        p = self.proxied(v)
-        self.assertEqual(p*2, v*2)
-        self.assertEqual(2*p, 2*v)
-        self.assertEqual(2**p, 2**v)
-        self.assertEqual(p**2, v**2)
-        self.assertEqual(-p, -v)
-        self.assertEqual(+p, +v)
-        for f in abs, int, long, float, hash, complex:
-            self.assertEqual(f(p), f(v))
-        self.assertEqual(p<22, v<22)
-        self.assertEqual(p>=10, v>=10)
-        self.assertEqual(p>9.0, v>9.0)
-        self.assertEqual(p<=9.25, v<=9.25)
-        self.assertEqual(p==7, v==7)
-        self.assertEqual(p!=18, v!=18)
+    def check_integer(self, v):
+        for op in (operator.and_, operator.or_, operator.xor):
+            self.check_func(op, v, 0b10101, reversable=True)
+        for op in (operator.lshift, operator.rshift):
+            self.check_func(op, v, 3, reversable=True)
+        for op in (operator.invert, hex, oct):
+            self.check_func(op, v)
 
-        self.assertEqual(16+p, 16+v)
-        self.assertEqual(62-p, 62-v)
-        self.assertEqual(p+16, v+16)
-        self.assertEqual(p-62, v-62)
-        self.assertEqual(p%7, v%7)
-        self.assertEqual(p/6, v/6)
+        self.check_numeric(v)
 
-        self.assertEqual(22<p, 22<v)
-        self.assertEqual(10>=p, 10>=v)
-        self.assertEqual(9.0>p, 9.0>v)
-        self.assertEqual(9.25<=p, 9.25<=v)
-        self.assertEqual(7==p, 7==v)
-        self.assertEqual(18!=p, 18!=v)
+    def check_numeric(self, v):
+        for op in (
+            operator.pos, operator.neg, abs, int, long, float, hash, complex
+        ):
+            self.check_func(op, v)
 
-        self.assertEqual(cmp(p,14), cmp(v,14))
-        self.assertEqual(cmp(14,p), cmp(14,v))
+        for other in (5, 13.7):  # Check against both an int and a float
+            for op in(operator.mul, operator.pow, operator.add, operator.sub):
+                self.check_func(op, v, other, reversable=True)
 
-        self.assertEqual(divmod(p,3), divmod(v,3))
+            for op in (
+                operator.lt, operator.le, operator.gt, operator.ge,
+                operator.eq, operator.ne, cmp
+            ):
+                self.check_func(op, v, other, reversable=True)
 
-        if v:
-            self.assertEqual(divmod(3,p), divmod(3,v))
-            self.assertEqual(62//p, 62//v)
-            self.assertEqual(7/p, 7/v)
-            self.assertEqual(7%p, 7%v)
+            for op in (
+                div, operator.truediv, operator.floordiv, operator.mod,
+                divmod
+            ):
+                self.check_func(op, v, other, reversable=v)
+                if not v:
+                    with self.assertRaises(ZeroDivisionError):
+                        op(other, self.proxied(v))
 
-        self.checkBasics(v)
+        self.check_basics(v)
 
-    def checkList(self, v):
+    def check_list(self, v):
         p = self.proxied(v)
         for i in range(len(v)):
             self.assertEqual(p[i], v[i])
             self.assertEqual(p[i:], v[i:])
             self.assertEqual(p[:i], v[:i])
             self.assertEqual(p[i::-1], v[i::-1])
-        self.checkContainer(v)
+        self.check_container(v)
 
         c = list(v)
         del p[::1]
@@ -193,37 +185,37 @@ class ProxyTestMixin:
         c[1:1] = [23]
         self.assertEqual(v, c)
 
-    def checkContainer(self, v):
+    def check_container(self, v):
         p = self.proxied(v)
         self.assertEqual(list(p), list(v))
         self.assertEqual(list(iter(p)), list(iter(v)))
         self.assertEqual(len(p), len(v))
         self.assertEqual(42 in p, 42 in v)
         self.assertEqual(99 in p, 99 in v)
-        self.checkBasics(v)
+        self.check_basics(v)
 
-    def checkBasics(self, v):
+    def check_basics(self, v):
         p = self.proxied(v)
         for f in bool, repr, str:
             self.assertEqual(f(p), f(v))
 
-    def testNumbers(self):
+    def test_numbers(self):
         for i in range(20):
-            self.checkInteger(i)
+            self.check_integer(i)
 
         f = -40
         while f<=20.0:
-            self.checkNumeric(f)
+            self.check_numeric(f)
             f += 2.25
 
-    def testLists(self):
+    def test_lists(self):
         for d in [1,2], [3,42,59], [99,23,55]:
-            self.checkList(d)
+            self.check_list(d)
 
 
 class InPlaceMixin(ProxyTestMixin):
 
-    def checkInteger(self, vv):
+    def check_integer(self, vv):
         mk = lambda: (self.proxied(vv), vv)
         p,v = mk()
         p|=0b010101; v|=0b010101
@@ -232,9 +224,9 @@ class InPlaceMixin(ProxyTestMixin):
         p,v = mk(); p^=0b010101; v^=0b010101; self.assertEqual(p.__subject__, v)
         p,v = mk(); p<<=3; v<<=3; self.assertEqual(p.__subject__, v)
         p,v = mk(); p>>=3; v>>=3; self.assertEqual(p.__subject__, v)
-        ProxyTestMixin.checkInteger(self, vv)
+        ProxyTestMixin.check_integer(self, vv)
 
-    def checkNumeric(self, vv):
+    def check_numeric(self, vv):
         mk = lambda: (self.proxied(vv), vv)
         p,v = mk(); p+=17; v+=17; self.assertEqual(p.__subject__, v)
         p,v = mk(); p-=22; v-=22; self.assertEqual(p.__subject__, v)
@@ -243,7 +235,7 @@ class InPlaceMixin(ProxyTestMixin):
         p,v = mk(); p**=2; v**=2; self.assertEqual(p.__subject__, v)
         p,v = mk(); p/=61; v/=61; self.assertEqual(p.__subject__, v)
         p,v = mk(); p%=19; v%=19; self.assertEqual(p.__subject__, v)
-        ProxyTestMixin.checkNumeric(self, vv)
+        ProxyTestMixin.check_numeric(self, vv)
 
 
 class TestLazyProxy(InPlaceMixin, unittest.TestCase):
